dbb9563e38730021c17487665cd149f9ccc2ae00
Fabio3rs <Fabio3rs@users.noreply.github.com>
2019-02-12 20:36:56 -0200

    Improvements, add Ctrl C handler code
    Set [gpu:0]/GPUFanControlState to 0 when exit with Ctrl + C command

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 // https://github.com/Fabio3rs
 // This code needs improvements
+// Use at your own risk
+// Use por sua conta e risco.
 #include <cstdint>
 #include <cstdio>
 #include <cstdlib>
@@ -9,74 +11,79 @@
 #include <chrono>
 #include <thread>
 #include <memory>
+#include <utility>
+#include <vector>
 
-class unique_file
+// For sigaction
+#include <signal.h>
+#include <unistd.h>
+
+
+// RAII for FILE*
+class CFiledtor
 {
-    FILE *fp;
-
 public:
-    FILE *get() const { return fp; }
-    void reset() { if (fp) fclose(fp); fp = nullptr; }
-	
-    operator bool() const { return fp != nullptr; }
-    
-    unique_file &operator=(const unique_file&) = delete;
-	
-    unique_file &operator=(unique_file &&u)
-	{
-		if (std::addressof(u) == this) return *this;
-		reset();
-		fp = std::move(u.fp); u.fp = nullptr; return *this;
-	}
-    
-    unique_file() : fp(nullptr) {}
-    unique_file(FILE *p) : fp(p) {}
-    unique_file(unique_file &&u) : fp(std::move(u.fp)) { u.fp = nullptr; }
-    ~unique_file() { if (fp) fclose(fp); }
+	void operator() (FILE *p) const { fclose(p); }
 };
+
+typedef std::unique_ptr<FILE, CFiledtor> unique_file;
+
+
+typedef std::vector<std::pair<std::string, std::string>> command_t;
 
 std::string getNvidiaAttribute(const char *attr)
 {
-    unique_file u(popen((std::string("/usr/bin/nvidia-settings -q ") + attr).c_str(), "r"));
+	char buff[1024] = { "/usr/bin/nvidia-settings -q " };
+	strcat(buff, attr);
+
+    unique_file u(popen(buff, "r"));
 
     if (u)
     {
         char path[1280];        
         
-        while (fgets(path, sizeof(path)-1, u.get()) != NULL) {
+        while (fgets(path, sizeof(path)-1, u.get()) != nullptr) {
             char *o = strstr(path, "): ");
             if (o) return std::string(o + sizeof("): ") - 1);
         }
-        return "Error";
+        return "";
     }else
     {
         printf("Failed to run command\n" );
-        return "";
+        return "Error";
     }
 }
 
-void setNvidiaAttribute(const char *attr, const char *value)
+void setNvidiaAttributes(const command_t &commands)
 {
-    std::string result = "/usr/bin/nvidia-settings -a ";
-    result += attr;
-    result += "=";
-    result += value;
-    
-    unique_file u(popen(result.c_str(), "r"));
-    
-    if (u)
-    {
-    }else
-    {
-        printf("Failed to run command\n" );
-    } 
+	if (commands.size() == 0)
+		return;
+
+	std::string commandLine = { "/usr/bin/nvidia-settings -a" };
+
+	for (auto &c : commands) { commandLine += " "; commandLine += c.first + "=" + c.second; }
+
+	unique_file u(popen(commandLine.c_str(), "r"));
+
+	if (u)
+	{   
+        char buff[1280];        
+        
+        while (fgets(buff, sizeof(buff) - 1, u.get()) != nullptr) {
+            char *o = strstr(buff, "ERROR");
+            if (o) printf("%s\n", buff);
+        }
+	}
+	else
+	{
+		printf("Failed to run command\n");
+	}
 }
 
-const int minspeed = 30;
-const int mintemp = 20;
-
-const int maxspeed = 100;
-const int maxtemp = 85;
+void setNvidiaAttribute(const std::string &attribute, const std::string &value)
+{
+	setNvidiaAttributes({ {attribute, value} });
+}
 
 int strtoint(const std::string &str, int defaultValue)
 {
@@ -89,42 +96,91 @@ int strtoint(const std::string &str, int defaultValue)
     return defaultValue;
 }
 
+void atexitThings(int s)
+{
+    printf("\n");
+
+    printf("Setting GPUFanControlState to 0...\n");
+    setNvidiaAttribute("[gpu:0]/GPUFanControlState", "0");
+    
+    printf("Exiting\n");
+    exit(1);
+}
+
+void catchExitSignal()
+{
+    // https://stackoverflow.com/questions/1641182/how-can-i-catch-a-ctrl-c-event
+    struct sigaction sigIntHandler;
+
+    sigIntHandler.sa_handler = atexitThings;
+    sigemptyset(&sigIntHandler.sa_mask);
+    sigIntHandler.sa_flags = 0;
+
+    sigaction(SIGINT, &sigIntHandler, NULL);
+}
+
+// TODO: Read the data from file
+// TODO: Possibility to create a speed curve
+// Data to calculate fan speed
+const int minspeed = 30;
+const int mintemp = 20;
+
+const int maxspeed = 100;
+const int maxtemp = 85;
+
 int main()
 {
-    printf("%s\n", getNvidiaAttribute("[gpu:0]/GPUCoreTemp").c_str());
-    printf("%s\n", getNvidiaAttribute("[gpu:0]/GPUFanControlState").c_str());
-    printf("%s\n", getNvidiaAttribute("[fan:0]/GPUTargetFanSpeed").c_str());
+    int GPUTemp = strtoint(getNvidiaAttribute("[gpu:0]/GPUCoreTemp"), 0);
+    printf("[gpu:0]/GPUCoreTemp         %d\n", GPUTemp);
+    printf("[gpu:0]/GPUFanControlState  %s\n", getNvidiaAttribute("[gpu:0]/GPUFanControlState").c_str());
+    printf("[fan:0]/GPUTargetFanSpeed   %s\n", getNvidiaAttribute("[fan:0]/GPUTargetFanSpeed").c_str());
     
+    printf("Setting GPUFanControlState to 1...\n");
     setNvidiaAttribute("[gpu:0]/GPUFanControlState", "1");
-        
+    
+    // handle Ctrl C
+    catchExitSignal();
+    
     const float deltaTemp = maxtemp - mintemp;
     const float deltaSpeed = maxspeed - minspeed;
     
     float speedTemp = deltaSpeed / deltaTemp;       
+
+    int lastGPUCoreTemp = GPUTemp + 1; // Make sure the fan speed calculations will run at least once
     
+    printf("GPU Temperature     Target fan speed\n");
     while (true)
     {
-        int GPUTemp = strtoint(getNvidiaAttribute("[gpu:0]/GPUCoreTemp"), 0);
+        GPUTemp = strtoint(getNvidiaAttribute("[gpu:0]/GPUCoreTemp"), 0);
         
-        if (GPUTemp < mintemp)
+        // Optimization to reduce the number of calls to nvidia-settings
+        if (lastGPUCoreTemp != GPUTemp)
         {
-            setNvidiaAttribute("[fan:0]/GPUTargetFanSpeed", std::to_string(minspeed).c_str());
-        }else if (GPUTemp >= maxtemp)
-        {
-            setNvidiaAttribute("[fan:0]/GPUTargetFanSpeed", std::to_string(maxspeed).c_str());
-        }else
-        {
-            float dtemp = (GPUTemp - mintemp);
-            float targetspeed = deltaSpeed;            
-            dtemp /= deltaTemp;
-            targetspeed *= dtemp;
-            targetspeed += minspeed;
-            
-            printf("%d %f %f\n", GPUTemp, dtemp, targetspeed);
-            setNvidiaAttribute("[fan:0]/GPUTargetFanSpeed", std::to_string((int)targetspeed).c_str());
+            if (GPUTemp < mintemp)
+            {
+                printf("%d                  %d\n", GPUTemp, minspeed);
+                setNvidiaAttribute("[fan:0]/GPUTargetFanSpeed", std::to_string(minspeed).c_str());
+            }else if (GPUTemp >= maxtemp)
+            {
+                printf("%d                  %d\n", GPUTemp, maxspeed);
+                setNvidiaAttribute("[fan:0]/GPUTargetFanSpeed", std::to_string(maxspeed).c_str());
+            }else
+            {
+                float dtemp = (GPUTemp - mintemp);
+                float targetspeed = deltaSpeed;            
+                dtemp /= deltaTemp;
+                targetspeed *= dtemp;
+                targetspeed += minspeed;
+                
+                int percentfanspeed = targetspeed;
+                
+                printf("%d                  %d\n", GPUTemp, percentfanspeed);
+                setNvidiaAttribute("[fan:0]/GPUTargetFanSpeed", std::to_string(percentfanspeed));
+            }
+            lastGPUCoreTemp = GPUTemp;
         }
         
-        std::this_thread::sleep_for(std::chrono::seconds(1));
+        std::this_thread::sleep_for(std::chrono::seconds(2));
     }
     
     return 0;
